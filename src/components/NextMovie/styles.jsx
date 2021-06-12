62815834632ed8b44983959b09ff8a71362883ed
Vanessa Lopes <63751937+NeuVanessa@users.noreply.github.com>
2021-06-12 15:16:00 -0300

    fix error gitignore

---
+++ src/components/NextMovie/styles.jsx
@@ -0,0 +1,41 @@
+import { Dimensions } from "react-native";
+import styled from "styled-components/native";
+
+const { height, width } = Dimensions.get("screen");
+
+export const Container = styled.View`
+  background-color: transparent;
+`;
+
+export const ContainerCarousel = styled.View`
+  height: 5%;
+`;
+
+export const Carousel = styled.View``;
+
+export const ContainerItens = styled.View`
+  height: 220px;
+  width: 120px;
+  background-color: #473f3f;
+  margin: 5px;
+`;
+
+export const Image = styled.Image.attrs({
+  resizeMode: "stretch",
+})`
+  width: 100%;
+  height: 70%;
+  border-bottom-left-radius: 100px;
+`;
+
+export const TextExemple = styled.Text`
+  font-size: 20px;
+  left: 4px;
+  font-family: monospace;
+`;
+export const TextDescription = styled.Text`
+  color: white;
+  font-size: 10px;
+  left: 4px;
+  font-family: monospace;
+`;
---
+++ src/components/PosterMovie/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useImperativeHandle } from "react";
+import { View, Text, FlatList } from "react-native";
+import {
+  Container,
+  ContainerCarousel,
+  Carousel,
+  ContainerItens,
+  Image,
+  TextExemple,
+  TextDescription,
+} from "./styles";
+import api from "../../services/api";
+export default function PosterMovie({ title }) {
+  //const lists = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const [latest, setLatest] = useState([]);
+  //https://api.themoviedb.org/3/
+  const apiKey = "api_key=2084a00f95fb3e4737567fae4e38ed85";
+  const language = "language=pt-BR";
+  const isMovies = async () => {
+    const response = await api.get(
+      `movie/now_playing?${apiKey}&${language}&page=3`
+    );
+    setLatest(response.data.results);
+  };
+  useEffect(() => {
+    isMovies();
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <ContainerCarousel></ContainerCarousel>
+
+        <TextExemple>{title}</TextExemple>
+
+        <FlatList
+          data={latest}
+          keyExtractor={(item, index) => `${index}`}
+          horizontal
+          renderItem={({ item }) => {
+            const uri = `https://image.tmdb.org/t/p/w342/${item.poster_path}`;
+
+            // console.log(uri);
+
+            return (
+              <ContainerItens>
+                <Image
+                  source={{
+                    uri,
+                  }}
+                />
+                <TextDescription>{item.title}</TextDescription>
+              </ContainerItens>
+            );
+          }}
+        />
+      </Container>
+    </>
+  );
+}
